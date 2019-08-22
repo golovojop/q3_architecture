bf5a7a6df9f6e261c5014dc18acad72692aa7251
yagray <yagray@mail.ru>
2019-08-22 12:45:15 +0300

    На проверку

--- Lesson3/src/main/java/FabricPatternApp.java
+++ Lesson3/src/main/java/FabricPatternApp.java
@@ -7,10 +7,12 @@ public class FabricPatternApp {
         HardwareManufacturer hm = new HardwareManufacturer();
 
         HardwareFactory hwfChina = hm.createFactory("china");
+        HardwareFactory hwfTaiwan = hm.createFactory("taiwan");
         HardwareFactory hwfOem = hm.createFactory("");
 
         System.out.println(hwfChina.createEthernetCard());
         System.out.println(hwfOem.createFiberCard());
         System.out.println(hwfOem.createEthernetCard());
+        System.out.println(hwfTaiwan.createWifiCard());
     }
 }
